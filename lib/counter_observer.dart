55d32f20e1ad11a476c3c290c15d356520a790f2
Denis Yurchenko <den4ik24@gmail.com>
2023-06-09 14:46:17 +0300

    first commit

---
+++ lib/counter_observer.dart
@@ -0,0 +1,12 @@
+import 'package:bloc/bloc.dart';
+
+class CounterObserver extends BlocObserver{
+  const CounterObserver();
+
+  @override
+  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
+    super.onChange(bloc, change);
+    // TODO: implement onChange
+    print("${bloc.runtimeType} $change");
+  }
+}
