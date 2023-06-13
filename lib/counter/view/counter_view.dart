eeead3eedc201c089d3ec2ec7d4698f8ef40e72c
Denis Yurchenko <den4ik24@gmail.com>
2023-06-13 22:17:06 +0300

    remake

--- lib/counter/view/counter_view.dart
+++ lib/counter/view/counter_view.dart
@@ -8,6 +8,7 @@ class CounterView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final cubit = context.read<CounterCubit>();
     return Scaffold(
       appBar: AppBar(title: const Text("Counter")),
       body: Center(
@@ -27,7 +28,7 @@ class CounterView extends StatelessWidget {
           FloatingActionButton(
             //key: const Key("counterView_increment_floatingActionButton"),
             child: const Icon(Icons.add),
-            onPressed: () => context.read<CounterCubit>().increment(),
+            onPressed: () => cubit.increment(),
           ),
           const SizedBox(
             height: 8,
@@ -35,7 +36,7 @@ class CounterView extends StatelessWidget {
           FloatingActionButton(
             //key: const Key("counterView_decrement_floatingActionButton"),
             child: const Icon(Icons.remove),
-            onPressed: () => context.read<CounterCubit>().decrement(),
+            onPressed: () => cubit.decrement(),
           ),
         ],
       ),
--- lib/counter_observer.dart
+++ lib/counter_observer.dart
@@ -2,11 +2,10 @@ import 'package:bloc/bloc.dart';
 
 class CounterObserver extends BlocObserver{
   const CounterObserver();
-
+  
   @override
   void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
     super.onChange(bloc, change);
-    // TODO: implement onChange
     print("${bloc.runtimeType} $change");
   }
 }
